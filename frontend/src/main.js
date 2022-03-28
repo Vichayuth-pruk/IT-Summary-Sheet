34969dfbc00c6d6b3126ed7f73bc571134b6f69c
Akira <54892411+docxed@users.noreply.github.com>
2022-03-28 20:42:44 +0700

    init

--- frontend/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import Router from "./router";
-import '@fortawesome/fontawesome-free/js/all.js';
-import './assets/css/styles.css'
-
-createApp(App).use(Router).mount("#app");
