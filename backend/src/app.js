6b051387cc5dcc71a82d3ef8509adf1cb6bbee16
Akira <54892411+docxed@users.noreply.github.com>
2022-05-22 03:16:53 +0700

    feat: docker compose

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }))
 const corsOptions = {
   origin:
     process.env.NODE_ENV === "production"
-      ? ["https://itsummarysheet.herokuapp.com"]
+      ? ["https://itsummarysheet.herokuapp.com", "http://localhost:443", "http://localhost:80"]
       : true,
   credentials: true,
 }
