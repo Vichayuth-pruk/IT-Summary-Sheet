e4a1820a5e17ce2edffd4f5202501f9d8f201142
aomsk <plaaom39@gmail.com>
2022-05-21 16:06:59 +0700

    feat: update myreview.jsx

--- frontend/src/pages/Myreview.jsx
+++ frontend/src/pages/Myreview.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useContext } from "react"
 import AuthContext from "../contexts/authContext"
 import isLoggedIn from "../middlewares/isLoggedIn"
 import { useNavigate } from "react-router-dom"
+import Rating from "@mui/material/Rating"
 
 function Myreview(props) {
   // Middleware
@@ -11,12 +12,56 @@ function Myreview(props) {
     isLoggedIn(props.meta, me, navigate)
   }, [])
 
+  //MockData
+  let reviewMock = [
+    {
+      '_id': '1',
+      'sheetName': 'COMPUTER PROGRAMMING (akira)',
+      'review': 'ดีมากกกกกกกกกกก',
+      'createdAt': '12 มีนาคม 2022 23:47:07',
+      'rating': 5
+    },
+    {
+      '_id': '2',
+      'sheetName': 'MULTIMEDIA TECHNOLOGY (docxed)',
+      'review': 'ดีโครตตตตตตตตตต',
+      'createdAt': '13 มีนาคม 2022 22:22:22',
+      'rating': 4.5
+    }
+  ]
   return (
     <>
       <div className="h2">รีวิวของฉัน</div>
       <hr />
       <br />
       <br />
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12">
+            {
+              reviewMock.map((item, index) => {
+                return (
+                  <div className='card mb-3 p-2' style={{ borderRadius: '15px', boxShadow: 'rgba(99, 99, 99, 0.2) 0px 2px 8px 0px' }} key={index}>
+                    <div className='card-body'>
+                      <h5 className='card-title'>{item.sheetName}</h5>
+                      <div>
+                        <Rating
+                          name="simple-controlled"
+                          defaultValue={item.rating}
+                          precision={0.5}
+                          readOnly
+                        />
+                      </div>
+                      <p className='card-text'>{item.review}</p>
+                        <p className='card-text text-end'>{item.createdAt}</p>
+                    </div>
+                  </div>
+                )
+              })
+            }
+          </div>
+        </div>
+      </div>
     </>
   )
 }
